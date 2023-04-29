3928440814d1d25542883179455298da09eaa727
Alexandr Kolganov <gengizar@yandex.ru>
2023-04-29 20:11:28 +0500

    feat(form): add drop zone

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,18 +3,20 @@
     <h1 class="">Заполните форму</h1>
     <div class="form__input">
       <label class="">Имя:</label>
-      <input type="text" required v-model="firstName" />
+      <input type="text" v-model="firstName" />
     </div>
+    <div v-if="v$.firstName.$invalid">Name field has an error.</div>
 
     <div class="form__input">
       <label class="">Фамилия:</label>
-      <input type="text" required v-model="secondName" />
+      <input type="text" v-model="lastName" />
     </div>
 
     <div class="form__input">
       <label class="">Email:</label>
-      <input type="email" required v-model="email" />
+      <input type="text" v-model="email" />
     </div>
+    <div v-if="v$.email.$error">Name field has an error.</div>
 
     <div class="form__input">
       <label>Категория:</label>
@@ -33,7 +35,7 @@
       Введите сюда ключевые слова через запятую, это поможет быстрее отработать ваш запрос
     </label>
     <div class="form__skills">
-      <label v-for="(word, index) in keyWords" :key="word" @click="onDelete(index)">{{
+      <label v-for="(word, index) in keyWords" :key="word" @click="onDeleteSkill(index)">{{
         word
       }}</label>
     </div>
@@ -43,26 +45,62 @@
       <textarea></textarea>
     </div>
 
+    <div
+      class="form__dropzone"
+      @dragenter.prevent="toggleActiveZone"
+      @dragleave.prevent="toggleActiveZone"
+      @dragover.prevent
+      @drop.prevent="onDrop"
+      :class="{ form__dropzone_active: activeZone }"
+    >
+      <span>Перенесите сюда файл для загрузки</span>
+      <span>или воспользуйтесь кнопкой</span>
+      <label for="dropzone">Загрузить</label>
+      <input type="file" id="dropzone" accept="image/*, .png, .jpg" @change="onDrop($event)" />
+      <span>Файлы формата <b>jpg/png</b>, размером не более <b>2мб</b></span>
+    </div>
+    <div class="form__dropfiles">
+      <div v-if="dropFiles.length">Загруженные файлы:</div>
+      <div v-for="(file, index) in dropFiles" :key="file.name">
+        <p>
+          {{ file.name }}
+        </p>
+        <span>/ {{ Math.floor(file.size / 1036) }}кб</span>
+        <button @click.prevent="onDeleteFile(index)">х</button>
+      </div>
+    </div>
+
     <div class="form__checkbox">
       <input type="checkbox" id="terms" v-model="terms" />
       <label for="terms">Соглашаюсь с обработкой персональных данных</label>
     </div>
 
-    <button type="submit">Отправить</button>
+    <button type="submit" class="form__submit-button">Отправить</button>
   </form>
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core';
+import { required, email, minLength } from '@vuelidate/validators';
+
 export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
       firstName: '',
-      secondName: '',
+      lastName: '',
       email: '',
       category: 'request',
       terms: 'false',
       tempKeyWords: '',
       keyWords: ['vvv', 'еще чет'],
+      dropFiles: [
+        { name: '2M6A5273.JPG', lastModified: 1679469630000, size: 5019239 },
+        { name: '2M6A5273.JPG', lastModified: 1679469630000, size: 5019239 },
+      ],
+      activeZone: false,
     };
   },
   methods: {
@@ -74,17 +112,58 @@ export default {
         this.tempKeyWords = '';
       }
     },
-    onDelete(index) {
+    onDeleteSkill(index) {
       this.keyWords.splice(index, 1);
+    },
+    toggleActiveZone() {
+      this.activeZone = !this.activeZone;
+    },
+    onDrop(e) {
+      const validation = (target) => {
+        if (this.dropFiles.some((el) => el.name === target.name) || target.size / 1036 > 2000) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      if (e.target.files) {
+        if (validation(e.target.files[0])) {
+          return;
+        } else {
+          this.dropFiles.push(e.target.files[0]);
+          console.log(e.target.files[0]);
+        }
+      } else if (e.dataTransfer.files) {
+        if (validation(e.dataTransfer.files[0])) {
+          this.activeZone = !this.activeZone;
+          return;
+        } else {
+          console.log(e.dataTransfer.files);
+          this.dropFiles.push(e.dataTransfer.files[0]);
+          this.activeZone = !this.activeZone;
+        }
+      }
+      console.log(this.dropFiles);
+    },
+    onDeleteFile(index) {
+      this.dropFiles.splice(index, 1);
     },
     onSubmit() {
       console.log(this.firstName);
     },
   },
+  validations() {
+    return {
+      firstName: { required, minLength: 5 },
+      lastName: { required },
+      email: { required, email }, // Matches this.contact.email
+    };
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .form {
   margin: 40px 0 30px;
   padding: 60px 20px;
@@ -130,22 +209,6 @@ export default {
     }
   }
 
-  &__checkbox {
-    width: 70%;
-    display: flex;
-    align-items: center;
-
-    label {
-      margin-left: 5px;
-      font-size: 0.9rem;
-      cursor: pointer;
-    }
-
-    input {
-      cursor: pointer;
-    }
-  }
-
   &__skills {
     width: 70%;
     margin-bottom: 10px;
@@ -186,6 +249,7 @@ export default {
     textarea {
       max-width: 100%;
       border: 1px solid rgba(0, 0, 0, 0.363);
+      margin-bottom: 10px;
       border-radius: 4px;
       padding: 4px;
       outline: none;
@@ -198,6 +262,118 @@ export default {
       &:focus {
         height: 120px;
       }
+    }
+  }
+
+  &__dropzone {
+    width: 40%;
+    height: 140px;
+    margin-bottom: 20px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    row-gap: 6px;
+    border: 2px dashed #41b883;
+    background-color: #fff;
+    transition: 0.3s ease all;
+
+    span {
+      text-align: center;
+    }
+
+    label {
+      margin-top: 4px;
+      padding: 8px 12px;
+      color: #fff;
+      background-color: #41b883;
+      transition: 0.3s ease all;
+      cursor: pointer;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+
+    input {
+      opacity: 0;
+      height: 0;
+      width: 0;
+      line-height: 0;
+      overflow: hidden;
+      padding: 0;
+      margin: 0;
+    }
+
+    &_active {
+      width: 45%;
+      height: 170px;
+      color: #fff;
+      border-color: #fff;
+      background-color: #41b883;
+
+      label {
+        background-color: #fff;
+        color: #41b883;
+      }
+    }
+  }
+
+  &__dropfiles {
+    display: flex;
+    width: 70%;
+    flex-direction: column;
+    margin-bottom: 10px;
+
+    div {
+      display: flex;
+      max-width: 65%;
+      margin-bottom: 2px;
+      border-radius: 10px;
+      white-space: nowrap;
+
+      &:first-of-type {
+        margin-bottom: 7px;
+        background-color: transparent;
+      }
+    }
+
+    p {
+      max-width: 60%;
+      margin: 0;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    span {
+      margin-right: 5px;
+    }
+
+    button {
+      margin-top: -3px;
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+    }
+  }
+
+  &__checkbox {
+    width: 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+
+    label {
+      margin-left: 5px;
+      font-size: 0.9rem;
+      cursor: pointer;
+    }
+
+    input {
+      cursor: pointer;
     }
   }
 }
